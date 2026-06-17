28d1c8d793be04f04d5cefc8023aa9dff6aec847
Vikas <vikas@example.com>
2026-06-17 16:46:12 +0530

    Update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app = FastAPI(title="DevForge BI Engine", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_origin_regex="https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
